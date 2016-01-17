67e614d93f9f2c0f559de55a6cdd5dd31ad5c642
Germangalia <germangalia@iesebre.com>
2016-01-17 19:25:59 +0100

    Implement method onCreate.

--- app/src/main/java/com/example/ggalia84/sensorsandroid/MainActivity.java
+++ app/src/main/java/com/example/ggalia84/sensorsandroid/MainActivity.java
@@ -7,9 +7,11 @@ import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
+    //Variables
     private SensorManager sensorManager = null;
 
     private Sensor sensorAcelerometro = null;
@@ -21,7 +23,38 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //Iniciant els sensors.
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        sensorAcelerometro = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorDeOrientacion = sensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION);
+
+        //Comprovar si hi ha sensors al dispositiu
+        if(sensorAcelerometro == null){
+            Toast.makeText(getApplicationContext(), "No hi ha sensor de acceleració.", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            Toast.makeText(getApplicationContext(), "Hi ha sensor de acceleració.", Toast.LENGTH_SHORT).show();
+            sensorManager.registerListener(this, sensorAcelerometro, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+
+        if(sensorDeOrientacion == null){
+            Toast.makeText(getApplicationContext(), "No hi ha sensor de Orientació", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            Toast.makeText(getApplicationContext(), "Hi ha sensor de Orientació", Toast.LENGTH_SHORT).show();
+            sensorManager.registerListener(this, sensorDeOrientacion, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+
+        //Inicialitzar els textview.
         setContentView(R.layout.activity_main);
+
+        textViewAcelerometro = (TextView) findViewById(R.id.sensorDeMovimiento);
+        textViewAcelerometro.setTextSize(30);
+
+        textViewOrientacion = (TextView) findViewById(R.id.sensorDeOrientacion);
+        textViewOrientacion.setTextSize(30);
     }
 
     @Override
