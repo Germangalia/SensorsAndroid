594c452b288006045a52a7d1ce68e32c90edca9c
Germangalia <germangalia@iesebre.com>
2016-01-17 19:11:41 +0100

    Add variables and override methods of SensorEvent Listener.

--- app/src/main/java/com/example/ggalia84/sensorsandroid/MainActivity.java
+++ app/src/main/java/com/example/ggalia84/sensorsandroid/MainActivity.java
@@ -1,13 +1,36 @@
 package com.example.ggalia84.sensorsandroid;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private SensorManager sensorManager = null;
+
+    private Sensor sensorAcelerometro = null;
+    private Sensor sensorDeOrientacion = null;
+
+    private TextView textViewAcelerometro = null;
+    private TextView textViewOrientacion = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
